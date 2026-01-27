8e99ba286a3c5b23a318ceb10c24643acecba9ca
wenggo <wengo@ierpsys.com>
2026-01-28 00:39:24 +0800

    style: format scripts/install.js to satisfy prettier

--- scripts/install.js
+++ scripts/install.js
@@ -2,8 +2,19 @@ const { spawnSync } = require('node:child_process')
 const { join } = require('node:path')
 
 function runNodePreGyp(cmd) {
-  const script = join(__dirname, '..', 'node_modules', '@mapbox', 'node-pre-gyp', 'bin', 'node-pre-gyp')
-  return spawnSync(process.execPath, [script, cmd], { stdio: 'inherit', env: process.env })
+  const script = join(
+    __dirname,
+    '..',
+    'node_modules',
+    '@mapbox',
+    'node-pre-gyp',
+    'bin',
+    'node-pre-gyp',
+  )
+  return spawnSync(process.execPath, [script, cmd], {
+    stdio: 'inherit',
+    env: process.env,
+  })
 }
 
 function run() {
@@ -15,7 +26,14 @@ function run() {
     }
     process.exit(0)
   } else {
-    const r = spawnSync('bash', [join(__dirname, 'install.sh')], { stdio: 'inherit', env: process.env })
+    const r = spawnSync(
+      'bash',
+      [join(__dirname, 'install.sh')],
+      {
+        stdio: 'inherit',
+        env: process.env,
+      },
+    )
     process.exit(r.status || 0)
   }
 }
